d49000cd05926b5a5e1d184b8667bea0cacfee25
Michael Kreil <github@michael-kreil.de>
2026-02-13 02:16:53 +0100

    feat: implement renderToSVG function for SVG rendering

--- src/demo.ts
+++ src/demo.ts
@@ -1,17 +1,16 @@
-import { Point2D } from './lib/geometry.js';
 import { styles } from '@versatiles/style';
-import { renderVectorTiles, SVGRenderer } from './index';
+import { renderToSVG } from './index';
 import { writeFileSync } from 'fs';
 
 const style = styles.colorful({});
 
-const svgString = await renderVectorTiles({
-	renderer: new SVGRenderer({ width: 512, height: 384, scale: 1 }),
+const svgString = await renderToSVG({
+	width: 512,
+	height: 384,
 	style,
-	view: {
-		center: new Point2D(13.408333, 52.518611),
-		zoom: 10,
-	},
+	lon: 13.408333,
+	lat: 52.518611,
+	zoom: 10,
 });
 
 writeFileSync(new URL('../docs/demo.svg', import.meta.url).pathname, svgString);
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,27 @@
-export { SVGRenderer } from './renderer/renderer_svg.js';
-export { renderVectorTiles } from './processor/render.js';
+import { SVGRenderer } from './renderer/renderer_svg.js';
+import { renderVectorTiles } from './processor/render.js';
+import { StyleSpecification } from '@maplibre/maplibre-gl-style-spec';
+import { Point2D } from './lib/geometry.js';
+
+export async function renderToSVG(options: {
+	width?: number;
+	height?: number;
+	scale?: number;
+	style: StyleSpecification;
+	lon?: number;
+	lat?: number;
+	zoom?: number;
+}): Promise<string> {
+	return await renderVectorTiles({
+		renderer: new SVGRenderer({
+			width: options.width ?? 1024,
+			height: options.height ?? 1024,
+			scale: options.scale ?? 1,
+		}),
+		style: options.style,
+		view: {
+			center: new Point2D(options.lon ?? 0, options.lat ?? 0),
+			zoom: options.zoom ?? 2,
+		},
+	});
+}
